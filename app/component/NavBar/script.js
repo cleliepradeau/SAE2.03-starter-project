fcd565a0f460c0b40aafdaf74bd78ae54d347414
cleliepradeau <clelie.pradeau@gmail.com>
2025-04-08 14:37:16 +0200

    iteration 6

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -3,10 +3,22 @@ let template = await templateFile.text();
 
 let NavBar = {};
 
-NavBar.format = function (hAbout, hProfile, ) {
+NavBar.format = function (hAbout, hProfile, profiles) {
   let html = template;
   html = html.replace("{{hAbout}}", hAbout);
   html = html.replace("{{handler}}", hProfile);
+
+  let profile = profiles
+  .map(
+    (p) =>
+      `<option value="${p.nom}" data-img="${p.image}" data-dob="${p.date_naissance}">${p.nom}</option>`
+  )
+  .join("");
+  
+  let image = profiles[0]?.image || "";
+  html = html.replace("{{profile}}", profile);
+  html = html.replace("{{image}}", image);
+
   return html;
 };
 
